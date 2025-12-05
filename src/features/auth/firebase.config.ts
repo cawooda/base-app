c34734e76a43dc144a3abf69b00362f763c74501
Andrew Cawood <cawooda@gmail.com>
2025-12-05 15:08:26 +1100

    fixed firebase.config gitignore

--- src/features/auth/firebase.config.ts
+++ src/features/auth/firebase.config.ts
@@ -4,8 +4,8 @@ const firebaseConfig = {
   projectId: 'circle-ced55',
   storageBucket: 'circle-ced55.firebasestorage.app',
   messagingSenderId: '574833378426',
-  appId: '1:574833378426:web:d218a6c67a55662b9d27c3',
-  measurementId: 'G-SDV0WZ7TD8',
+  appId: '1:574833378426:web:c2cbc02e845368559d27c3',
+  measurementId: 'G-THGTL08LZX',
 };
 
 export { firebaseConfig };
